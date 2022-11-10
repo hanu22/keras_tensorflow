438dc1f706de5ea28393935b54f0edca22ba26f5
sakshi <sakshi.dhama@brandbank.online>
2022-11-10 23:33:33 +0000

    Update delete function

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+#%%
 import os
 from pathlib import Path
 
@@ -6,21 +7,20 @@ from tensorflow import keras
 from tensorflow.keras import layers
 
 num_skipped = 0
-
+#%%
 folder_path = Path("PetImages")
+#%%
 for fname in folder_path.rglob("*.jpg"):
-    fpath = Path(folder_path, fname)
+    fpath = Path(fname)
     print(fpath)
-    try:
-        fobj = open(fpath, "rb")
-        is_jfif = tf.compat.as_bytes("JFIF") in fobj.peek(10)
-    finally:
-        fobj.close()
 
-    if not is_jfif:
-        num_skipped += 1
-        # Delete corrupted image
-        os.remove(fpath)
+    with fpath.open("rb") as fobj:
+        is_jfif = tf.compat.as_bytes("JFIF") in fobj.peek(10)
+
+        if not is_jfif:
+            num_skipped += 1
+            # Delete corrupted image
+            os.remove(fpath)
 
 print("Deleted %d images" % num_skipped)
 #%%
